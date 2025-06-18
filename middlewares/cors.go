388dad4e213eda92a6d5e4a1bd284459d1e3232b
Bruno Tarditi <brunotarditi@hotmail.com>
2025-06-18 19:09:23 -0300

    Se añade middlware para cors origin

---
+++ middlewares/cors.go
@@ -0,0 +1,32 @@
+package middlewares
+
+import (
+	"strings"
+
+	"github.com/gin-gonic/gin"
+)
+
+func CORSMiddleware(allowedOrigins []string) gin.HandlerFunc {
+
+	allowed := make(map[string]bool)
+	for _, origin := range allowedOrigins {
+		allowed[strings.TrimSpace(origin)] = true
+	}
+	return func(c *gin.Context) {
+		origin := c.GetHeader("Origin")
+
+		if allowed[origin] {
+			c.Header("Access-Control-Allow-Origin", origin)
+			c.Header("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE")
+			c.Header("Access-Control-Allow-Headers", "Origin, Content-Type, Accept, Authorization")
+			c.Header("Access-Control-Allow-Credentials", "true")
+		}
+
+		if c.Request.Method == "OPTIONS" {
+			c.AbortWithStatus(204)
+			return
+		}
+
+		c.Next()
+	}
+}
