45b9847a248346f737188b35891259682c18b84c
Bruno Tarditi <brunotarditi@hotmail.com>
2025-08-03 20:36:55 -0300

    Fixed ruta absoluta para enviar email

--- utils/renderhtml.go
+++ utils/renderhtml.go
@@ -2,17 +2,28 @@ package utils
 
 import (
 	"bytes"
+	"fmt"
 	"html/template"
+	"os"
+	"path/filepath"
 )
 
 func RenderVerificationEmail(path string, data any) (string, error) {
-	tmpl, err := template.ParseFiles(path)
+	wd, err := os.Getwd()
 	if err != nil {
-		return "", err
+		return "", fmt.Errorf("no se pudo obtener el directorio actual: %w", err)
 	}
+
+	fullPath := filepath.Join(wd, path)
+
+	tmpl, err := template.ParseFiles(fullPath)
+	if err != nil {
+		return "", fmt.Errorf("error al parsear template: %w", err)
+	}
+
 	var buf bytes.Buffer
 	if err := tmpl.Execute(&buf, data); err != nil {
-		return "", err
+		return "", fmt.Errorf("error al ejecutar template: %w", err)
 	}
 
 	return buf.String(), nil
