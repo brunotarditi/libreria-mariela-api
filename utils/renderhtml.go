9e51f3033de88cc01e242204898d645c4436dbb4
Bruno Tarditi <brunotarditi@hotmail.com>
2025-08-03 21:13:25 -0300

    se modifica docker file para copiar la carpeta templates

--- utils/renderhtml.go
+++ utils/renderhtml.go
@@ -2,29 +2,17 @@ package utils
 
 import (
 	"bytes"
-	"fmt"
 	"html/template"
-	"os"
-	"path/filepath"
 )
 
 func RenderVerificationEmail(path string, data any) (string, error) {
-	wd, err := os.Getwd()
+	tmpl, err := template.ParseFiles(path)
 	if err != nil {
-		return "", fmt.Errorf("no se pudo obtener el directorio actual: %w", err)
+		return "", err
 	}
-
-	fullPath := filepath.Join(wd, path)
-
-	tmpl, err := template.ParseFiles(fullPath)
-	if err != nil {
-		return "", fmt.Errorf("error al parsear template: %w", err)
-	}
-
 	var buf bytes.Buffer
 	if err := tmpl.Execute(&buf, data); err != nil {
-		return "", fmt.Errorf("error al ejecutar template: %w", err)
+		return "", err
 	}
-
 	return buf.String(), nil
 }
